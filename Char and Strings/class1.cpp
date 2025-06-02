f7369f456ff8d22f6f9fe1b11dbe7131b7b61a6f
Dev-Ankit-Ks <devankit.ks@gmail.com>
2025-06-02 21:49:40 +0530

    Char and Strings

---
+++ Char and Strings/class1.cpp
@@ -0,0 +1,124 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <limits.h>
+#include <string.h>
+using namespace std;
+#include <vector>
+
+
+int main() {
+  // creating array and taking input
+  // char arr[10];
+  // cout << "Enter your name : " << endl;
+  // cin.getline(arr, 50);
+  // cout << arr << endl; 
+  //---------------------------------------------------------------
+  // Question -> length of string(Custum)
+  // char arr[29];
+
+  // cin >> arr;
+
+  // int i = 0;
+  // int lenght = 0;
+
+  // while (arr[i] != '\0') {
+  //   lenght++;
+  //   i++;
+  // }
+  //---------------------------------------------------------------
+  // Question -> length of string(Library)
+  // cout << lenght << endl;
+
+  // char arr[29];
+
+  // cin >> arr;
+
+  // cout << strlen(arr) << endl;
+  //---------------------------------------------------------------
+  // Question -> reverse a string
+  // char arr[29];
+
+  // cin >> arr;
+
+  // int size = strlen(arr);
+
+  // cout << "Before Reverse" << endl;
+  // cout << arr << endl;
+  // cout << "After Reverse" << endl;
+  // for (int i = size; i >= 0; i--) {
+  //   cout << arr[i];
+  // }
+  //---------------------------------------------------------------
+  // Question -> replace all spaces
+  // char arr[29];
+
+  // cin.getline(arr, 29);
+
+  // int size = strlen(arr);
+
+  // for (int i = 0; i <= size - 1; i++) {
+  //   if (arr[i] == ' ') {
+  //     arr[i] = '@';
+  //   }
+  // }
+
+  // cout << arr << endl;
+  //---------------------------------------------------------------
+  // Question -> Palindrome
+  // char arr[29];
+
+  // cin.getline(arr, 29);
+
+  // bool result = palend(arr);
+
+  // bool palend(char arr[29]) {
+  //   int i = 0;
+  //   int n = strlen(arr);
+  //   int j = n - 1;
+
+  //   while (i <= j) {
+  //     if (arr[i] != arr[j]) {
+  //       return false;
+  //     } else {
+  //       i++;
+  //       j--;
+  //     }
+  //   }
+  //   return true;
+  // }
+
+  // cout << result << endl;
+  //---------------------------------------------------------------
+  // Question -> lowercase to uppercase
+  // Question -> uppercase to lowercase
+  // void convertIntoUpperCase(char arr[50]) {
+  //   cin.getline(arr, 50);
+  //   int n = strlen(arr);
+
+  //   for (int i = 0; i <= n - 1; i++) {
+  //     arr[i] = arr[i] - 'a' + 'A';
+  //   }
+
+  //   cout << arr;
+  // }
+
+  // void convertIntoLowerCase(char arr[50]) {
+  //   cin.getline(arr, 50);
+  //   int n = strlen(arr);
+
+  //   for (int i = 0; i <= n - 1; i++) {
+  //     arr[i] = arr[i] - 'A' + 'a';
+  //   }
+
+  //   cout << arr;
+  // }
+  // char arr[50];
+  // convertIntoLowerCase(arr);
+  // convertIntoUpperCase(arr);
+  //---------------------------------------------------------------
+   
+
+  return 0;
+}
+ 
