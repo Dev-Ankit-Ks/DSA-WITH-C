27557f909950991cadcfa309878c2e3699ae2576
Dev-Ankit-Ks <devankit.ks@gmail.com>
2025-06-05 16:07:48 +0530

    Char and Strings

--- Char and Strings/class2.cpp
+++ Char and Strings/class2.cpp
@@ -5,6 +5,7 @@
 #include <string.h>
 using namespace std;
 #include <vector>
+//------------------------------------------------------
 // bool checkPalin(string s, int i, int j) {
 //   while (i <= j) {
 //     if (s[i] != s[j]) {
@@ -31,7 +32,7 @@ using namespace std;
 //   }
 //   return true;
 // }
-
+//------------------------------------------------------
 int main() {
   // Question -> Adjcent Character
   // string ans = "";
@@ -63,8 +64,39 @@ int main() {
   // }
   // cout << s << endl;
   //------------------------------------------------------
-  // Question -> Valid Palendrome ||
+  // Question -> Valid Palendrome || (Not Clear)
   // bool ans = validPalen("abca");
   // cout << boolalpha << ans << endl;
+  //------------------------------------------------------
+  // Question -> Minimum Time Differnce
+  // vector<string> timePoints = {"23:59", "00:00"};
+  // vector<int> minutes;
+
+  // for (int i = 0; i < timePoints.size(); i++) {
+  //   string curr = timePoints[i];
+  //   int hour = stoi(curr.substr(0, 2));
+  //   int min = stoi(curr.substr(3, 2));
+  //   int total_minutes = hour * 60 + min;
+  //   minutes.push_back(total_minutes);
+  // }
+
+  // sort(minutes.begin(), minutes.end());
+
+  // int mini = INT_MAX;
+  // int n = minutes.size() - 1;
+
+  // for (int i = 0; i < n; i++) {
+  //   int diff = minutes[i + 1] - minutes[i];
+  //   mini = min(mini, diff);
+  // }
+
+  // int lastDiff = (minutes[0] + 1440) - minutes[n];
+  // mini = min(mini, lastDiff);
+
+  // cout << mini << endl;
+  //------------------------------------------------------
+  // Question Palindromic Substring
+  //<Start From Here...>
+
   return 0;
 }
