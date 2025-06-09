962a8683883649c2a7515b45f311d615a0a568ac
Dev-Ankit-Ks <devankit.ks@gmail.com>
2025-06-09 23:09:06 +0530

    Char and Strings

---
+++ Char and Strings/homework.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <limits.h>
+#include <string.h>
+using namespace std;
+#include <vector>
+
+int main() {
+  // Question Valid Anagram
+
+  //---------------------------------------------
+  // Question -> Reverse Only Letters
+  
+  // string s = "Test1ng-leet=codeQ!";
+
+  // int i = 0;
+  // int j = s.length() - 1;
+
+  // while (i < j) {
+  //   if (isalpha(s[i]) && isalpha(s[j])) {
+  //     swap(s[i], s[j]);
+  //     i++, j--;
+  //   } else if (!isalpha(s[i])) {
+  //     i++;
+  //   } else {
+  //     j--;
+  //   }
+  // }
+
+  // cout << s;
+  //---------------------------------------------
+  // Question -> Longest Common prefix
+
+  //---------------------------------------------
+  // Question -> Reverse Vowels of a string
+  
+
+
+  return 0;
+}
