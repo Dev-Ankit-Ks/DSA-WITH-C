cc2273a207152603f8b658660cf5f46e4ec88939
Dev-Ankit-Ks <devankit.ks@gmail.com>
2025-07-04 01:41:35 +0530

    class 2

---
+++ Pointers/class2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //   arr
+  // &arr[0]
+  //   &arr
+
+  // above all give base address
+
+  //-----------------------------------------------------------------
+
+  // int arr[5] = {2, 4,8,9,7};
+
+  // cout << *arr << endl;//2
+  // cout << arr[0]<< endl;//2
+  // cout << *arr + 1 << endl;//3
+  // cout << *(arr) + 1 << endl;//3
+  // cout <<*(arr + 1) << endl;//4
+  // cout<< arr[1]<< endl;//4
+  // cout <<*(arr + 2) << endl;//8
+  // cout<< arr[2]<< endl;//8
+  // cout << *(arr + 3) << endl;//9
+
+  // at the end arr[2] = *(arr + 2)
+  //------------------------------------------------------------------
+  // arr[i] = i[arr] = *(arr + i) = *(i+arr)
+  //------------------------------------------------------------------
+}
